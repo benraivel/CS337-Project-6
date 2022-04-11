964283c48121d63a52556c6648248ba8f7c212ad
benraivel <benraivel@gmail.com>
2022-04-11 18:06:03 -0400

    All locks working, need demonstrations

--- bakery.py
+++ bakery.py
@@ -6,9 +6,9 @@ class Bakery:
         '''
         create choosing and tickets list
         '''
-        
+        self.n_threads = thread_count
         self.choosing = np.empty(thread_count, dtype=bool)
-        self.tickets = np.empty(thread_count, dtype=int)
+        self.tickets = np.zeros(thread_count)
 
 
     def lock(self, thread_ID):
@@ -16,7 +16,22 @@ class Bakery:
         
         '''
         # thread wants a ticket
-        self.choosing[thread_ID] == True
+        self.choosing[thread_ID] = True
 
         # give ticket
         self.tickets[thread_ID] = np.max(self.tickets) + 1
+
+        # got ticket
+        self.choosing[thread_ID] = False
+
+        for i in range(self.n_threads):
+
+            while self.choosing[i]: pass
+            
+            while (self.tickets[i] != 0) and ((self.tickets[i] < self.tickets[thread_ID]) or ((self.tickets[i] == self.tickets[thread_ID]) and (i<thread_ID))): pass
+
+
+    def unlock(self, thread_ID):
+
+        self.tickets[thread_ID] = 0
+        #print('unlocked!')
--- peterson.py
+++ peterson.py
@@ -1,18 +1,29 @@
-import math
+# Ben Raivel
+# CS337
+
+# implements a spinlock using peterson's synchronization solution
 
 class Peterson:
 
-    flag = [False, False]
-    turn = 0
+    def __init__(self):
+        '''
+        create instance variables for flag, turn
+        '''
+        self.flag = [False, False]
+        self.turn = 0
 
     def lock(self, Thread_ID):
-        global flag
-        global turn
-        flag[Thread_ID] = True
-        turn = abs(Thread_ID-1)
+        '''
+        acquire a lock for Thread_ID
+        '''
+        other_thread = abs(Thread_ID-1)
+        self.flag[Thread_ID] = True
+        self.turn = other_thread
 
-        while self.turn == 1 and flag[abs(Thread_ID-1)]: pass
+        while self.turn == other_thread and self.flag[other_thread]: pass
 
     def unlock(self, Thread_ID):
-        global flag
-        flag[Thread_ID] = False
+        '''
+        release lock held by Thread_ID
+        '''
+        self.flag[Thread_ID] = False
